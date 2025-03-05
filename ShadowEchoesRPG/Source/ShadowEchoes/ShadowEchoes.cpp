fa5b2fce2d277720b69158db27c2bd5868a444f7
Emerson Alex Ribeiro Sbeghen Filho <104532764+Emerson-Alex@users.noreply.github.com>
2025-03-05 12:07:25 -0300

    Add ShadowEchoesRPG/Source/ShadowEchoes/ShadowEchoes.cpp 1741187245662

---
+++ ShadowEchoesRPG/Source/ShadowEchoes/ShadowEchoes.cpp
@@ -0,0 +1,6 @@
+// Copyright Shadow Echoes RPG. All Rights Reserved.
+
+#include "ShadowEchoes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, ShadowEchoes, "ShadowEchoes");
